cdc87a300054faf28bcff20a404751f72df906dd
qiaobaaa <49155648+qiaobaaa@users.noreply.github.com>
2026-02-28 02:44:21 +0800

    Update My_Nes.ino

--- My_Nes.ino
+++ My_Nes.ino
@@ -1,5 +1,14 @@
 void setup() {
-  // put your setup code here, to run once:
+  /  /*
+  MALLOC_CAP_SPIRAM
+→ 只从 PSRAM 分配
+MALLOC_CAP_INTERNAL
+→ 只从内部 RAM分配
+MALLOC_CAP_DEFAULT
+→ 自动分配（先内部，不够再 PSRAM）
+  */
+  nes.cartridge.prg_rom = (uint8_t *)heap_caps_malloc(nes.cartridge.prg_rom_size, MALLOC_CAP_SPIRAM);
+  nes.cartridge.chr_rom = (uint8_t *)heap_caps_malloc(nes.cartridge.chr_rom_size, MALLOC_CAP_SPIRAM);
 
 }
 
